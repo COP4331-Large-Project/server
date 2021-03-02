6a4802cc405dba63acbc59d0dad48dbd5291a049
beedude <suneettipirneni@icloud.com>
2021-03-02 14:07:17 -0500

    add rate limiter

--- server/src/modules/react-web.js
+++ server/src/modules/react-web.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import fs from 'fs';
 import __dirname from './globals.js';
 
@@ -10,12 +11,21 @@ import __dirname from './globals.js';
  * @param {Express} app The express app.
  */
 function initStaticWebFiles(app) {
+  const rateLimiter = rateLimit({
+    windowMs: 0.25 * 1000, // 1/4 second
+    max: 5,
+  });
   const filePath = path.join(__dirname, '../../../client/build');
+
+  // Apply the rate limiter
+  app.use(rateLimiter);
 
   // Check if website has been built
   if (!fs.existsSync(filePath)) {
     console.error('Skipping setting up react web files...');
-    console.error('Run \'npm run build\' in the client directory to upload the react website.');
+    console.error(
+      "Run 'npm run build' in the client directory to upload the react website.",
+    );
     return;
   }
 
