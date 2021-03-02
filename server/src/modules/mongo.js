6ee7a7850feec15ebe534f6f6e8862912d07f7ac
beedude <suneettipirneni@icloud.com>
2021-03-02 18:32:44 -0500

    Revert "added connection to Atlas"
    This reverts commit e23579b9
    
    Signed-off-by: beedude <suneettipirneni@icloud.com>

--- server/src/modules/mongo.js
+++ server/src/modules/mongo.js
@@ -8,19 +8,7 @@ const connectToDB = () => {
   console.log(`Host = ${process.env.HOST}`);
   console.log(`User = ${process.env.USER}`);
   console.log(`Password = ${process.env.PASSWORD}`);
-  console.log(`dbName = ${process.env.DBNAME}`);
-
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}
-               ${process.env.HOST}/${process.env.DBNAME}?retryWrites=true&w=majority`;
-
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-  });
+  console.log(`Port = ${process.env.PORT}`);
 };
 
 export default connectToDB;
