eb2790c2fc3d37f7fffd98c7ff1c3a3e260d6ef2
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-03-02 18:03:16 -0500

    added connection to Atlas

--- server/src/modules/mongo.js
+++ server/src/modules/mongo.js
@@ -8,7 +8,19 @@ const connectToDB = () => {
   console.log(`Host = ${process.env.HOST}`);
   console.log(`User = ${process.env.USER}`);
   console.log(`Password = ${process.env.PASSWORD}`);
-  console.log(`Port = ${process.env.PORT}`);
+  console.log(`dbName = ${process.env.DBNAME}`);
+
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}
+               ${process.env.HOST}/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
 };
 
 export default connectToDB;
