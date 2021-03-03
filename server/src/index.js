8f1824e0af76b19b2549d03d1461e2fb37f99ad3
beedude <suneettipirneni@icloud.com>
2021-03-03 07:46:59 -0500

    Missed a console.log
    Signed-off-by: beedude <suneettipirneni@icloud.com>

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { logger } from './modules/globals.js';
 import initStaticWebFiles from './modules/react-web.js';
 import connectToDB from './modules/mongo.js';
 
 const app = express();
 initStaticWebFiles(app);
 
-console.log('Server is running on http://localhost:8080');
+logger.info('Server is running on http://localhost:8080');
 
 app.listen(process.env.PORT || 3000);
 
