c3ea1d2a6f03e3caa4e66fce5a5a79f739132de5
beedude <suneettipirneni@icloud.com>
2021-03-04 15:55:30 -0500

    Add initial scss code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../scss/App.scss';
+import '../scss/Container.scss';
+import '../scss/Button.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Gradient-Container">
+        <div className="Rotated-Container Gradient-Box">
           <div className="Card">
             <h1>COP4331C Large Group #7</h1>
             <a
-              className="accent"
+              className="btn btn-primary"
               href="https://github.com/COP4331-Large-Project/website"
               target="_blank"
               rel="noopener noreferrer"
---
+++ client/src/components/Catalogue.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../scss/Button.scss';
+import '../scss/App.scss';
+
+function Catalogue() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        justifyItems: 'center',
+      }}
+    >
+      <h1>Buttons</h1>
+      <div className="Display-Box">
+        <button className="btn btn-primary">.btn btn-primary</button>
+        <button className="btn btn-danger">.btn btn-danger</button>
+        <button className="btn btn-success">.btn btn-success</button>
+        <button className="btn btn-warning">.btn btn-warning</button>
+      </div>
+      <h1>Card</h1>
+      <div className="Display-Box">
+        <div className="Card">.Card - Displays a Block of content</div>
+      </div>
+      <h1>Gradient Box</h1>
+      <div className="Gradient-Box">Gradient Box - Lorem Ipsum</div>
+      <h1>Rotated Container</h1>
+      <div className="Rotated-Container" style={{ border: '1px solid red' }}>
+        <div className="Card"> My Parent is Rotated but I am not</div>
+      </div>
+      <h1>Card + Rotated Container + Gradient Box</h1>
+      <div className="Gradient-Box Rotated-Container">
+        <div className="Card">Hello World!</div>
+      </div>
+    </div>
+  );
+}
+
+export default Catalogue;
