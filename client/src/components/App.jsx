ba79152dcd09008e75f4102ea79af8642a85ac80
beedude <suneettipirneni@icloud.com>
2021-03-08 15:39:28 -0500

    Remove component code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import '../scss/App.scss';
-import '../scss/Container.scss';
-import '../scss/Button.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Catalogue from './Catalogue.jsx';
-import Button from './Button.jsx';
-
-function goToGithub() {
-  window.location.href = 'https://github.com/COP4331-Large-Project/website';
-}
 
 function App() {
   return (
@@ -21,16 +13,11 @@ function App() {
               <div className="Rotated-Container Gradient Box">
                 <div className="Card">
                   <h1>COP4331C Large Group #7</h1>
-                  <Button variant={'dark'} onClick={goToGithub}>
-                    <i className="bi-github"> </i>
-                    Visit Our GitHub
-                  </Button>
                 </div>
               </div>
             </header>
           </div>
         </Route>
-        <Route exact path="/catalogue" component={Catalogue} />
       </Switch>
     </Router>
   );
