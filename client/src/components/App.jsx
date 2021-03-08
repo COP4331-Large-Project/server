d6c3f19c9f0fdfe7e8ed2a238f99f47bf5531697
beedude <suneettipirneni@icloud.com>
2021-03-08 16:14:02 -0500

    Add button component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import '../scss/App.scss';
+import '../scss/button.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Button from './Button.jsx';
+
+function goToGithub() {
+  window.location.href = 'https://github.com/COP4331-Large-Project';
+}
 
 function App() {
   return (
@@ -14,6 +20,10 @@ function App() {
                 <div className="Card">
                   <h1>COP4331C Large Group #7</h1>
                 </div>
+                <Button variant="dark" onClick={goToGithub}>
+                  <i className="bi bi-github"> </i>
+                  Visit Out Github
+                </Button>
               </div>
             </header>
           </div>
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button({ variant, children, onClick }) {
+  return (
+    <button onClick={onClick} type="button" className={`btn btn-${variant}`}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  variant: 'primary',
+  children: [],
+  onClick: () => {},
+};
+
+Button.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+};
+
+export default Button;
