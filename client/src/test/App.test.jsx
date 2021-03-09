4ff8332d51955e8aaf0bf67545cd695d511cef78
beedude <suneettipirneni@icloud.com>
2021-03-09 17:39:22 -0500

    Initial Commit

--- client/src/test/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../components/App.jsx';
-
-test('renders github button', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Visit Our Github/i);
-  expect(linkElement).toBeInTheDocument();
-});
