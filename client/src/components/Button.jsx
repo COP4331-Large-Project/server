f0a8d09efe19f9f73842bf0e3e01c13cdd13b082
beedude <suneettipirneni@icloud.com>
2021-03-08 22:37:08 -0500

    Fix prettier fighting with eslint and add classname to Button.jsx

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ variant, children, onClick }) {
+function Button({
+  className, variant, children, onClick,
+}) {
   return (
-    <button onClick={onClick} type="button" className={`btn btn-${variant}`}>
+    <button
+      onClick={onClick}
+      type="button"
+      className={`btn btn-${variant} ${className}`}
+    >
       {children}
     </button>
   );
@@ -13,12 +19,14 @@ Button.defaultProps = {
   variant: 'primary',
   children: [],
   onClick: () => {},
+  className: '',
 };
 
 Button.propTypes = {
   variant: PropTypes.string,
   children: PropTypes.node,
   onClick: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default Button;
