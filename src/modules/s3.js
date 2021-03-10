758e7a0aed5455c46e6340a934b9e6ebaac1b17e
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-10 17:08:47 -0500

    Rectified credentials issue with s3

--- src/modules/s3.js
+++ src/modules/s3.js
@@ -4,17 +4,11 @@ import dotenv from 'dotenv';
 const REGION = "us-east-1";
 dotenv.config();
 
-const s3 = new S3Client({
-    region: REGION,
-    credentials:{
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
-});
+const s3 = new S3Client({ region: REGION });
 
 const listBuckets = async () => {
     var data;
-    try{
+    try {
         data = await s3.send(new ListBucketsCommand({}));
         console.log("Success", data.Buckets);
     } catch (err) {
