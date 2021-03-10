18237cd7d5a382b564e5de58b5763e9c3b0e522e
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-10 17:29:20 -0500

    Linter

--- src/modules/s3.js
+++ src/modules/s3.js
@@ -1,20 +1,21 @@
-import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
+import { logger } from './globals.js';
 
-const REGION = "us-east-1";
+const REGION = 'us-east-1';
 dotenv.config();
 
 const s3 = new S3Client({ region: REGION });
 
 const listBuckets = async () => {
-    var data;
-    try {
-        data = await s3.send(new ListBucketsCommand({}));
-        console.log("Success", data.Buckets);
-    } catch (err) {
-        console.log("Error", err);
-    }
-    return data
-}
-export default listBuckets;
+  let data;
+  try {
+    data = await s3.send(new ListBucketsCommand({}));
+    logger.log('Success', data.Buckets);
+  } catch (err) {
+    logger.log('Error', err);
+  }
+  return data;
+};
 
+export default listBuckets;
