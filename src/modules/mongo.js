2dc463e45e6ef2c1722f8498197c13657ad3bdbb
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-10 16:29:00 -0500

    Fixed missing _

--- src/modules/mongo.js
+++ src/modules/mongo.js
@@ -6,7 +6,7 @@ const connectToDB = () => {
   dotenv.config();
 
   // Log the values.
-  logger.info(`MongoDB URI = ${process.env.MONGO_URI}`);
+  logger.info(`MongoDB_URI = ${process.env.MONGODB_URI}`);
 };
 
 export default connectToDB;
