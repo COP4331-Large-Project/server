091ca688646c96ae71fd819a8f8e3e3c4c4fe3bf
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-10 16:37:09 -0500

    Changed verification method

--- src/routes/s3-listbuckets.js
+++ src/routes/s3-listbuckets.js
@@ -2,8 +2,15 @@ import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
 import dotenv from 'dotenv';
 
 const REGION = "us-east-1";
+dotenv.config();
 
-const s3 = new S3Client({region: REGION});
+const s3 = new S3Client({
+    region: REGION,
+    credentials:{
+        accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
 
 const listBuckets = async () => {
     var data;
