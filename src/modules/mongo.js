2d2bfc055a9dab86b2a3cda6446cea075c4301b7
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-03-09 22:59:43 -0500

    Revert "added the db connect and disconnect functions"
    This reverts commit 038cd1a970005d1d3ae7830e3ba9784de5718433.

--- src/modules/mongo.js
+++ src/modules/mongo.js
@@ -1,21 +1,12 @@
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import { logger } from './globals.js';
 
 const connectToDB = () => {
   // Load in environment variables from file.
   dotenv.config();
 
-  // Create the DB connection.
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-
   // Log the values.
   logger.info(`MongoDB URI = ${process.env.MONGO_URI}`);
 };
 
-const disconnectFromDB = () => {
-  // Close the DB connection.
-  mongoose.connection().close();
-};
-
-export { connectToDB, disconnectFromDB };
+export default connectToDB;
