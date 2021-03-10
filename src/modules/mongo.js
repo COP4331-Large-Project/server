0b72b1b78b21a56bd5367d9e7532648ea52c41c1
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-03-09 23:01:39 -0500

    added the db connect and disconnect functions

--- src/modules/mongo.js
+++ src/modules/mongo.js
@@ -1,12 +1,21 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { logger } from './globals.js';
 
 const connectToDB = () => {
   // Load in environment variables from file.
   dotenv.config();
 
+  // Create the DB connection.
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
   // Log the values.
   logger.info(`MongoDB URI = ${process.env.MONGO_URI}`);
 };
 
-export default connectToDB;
+const disconnectFromDB = () => {
+  // Close the DB connection.
+  mongoose.connection().close();
+};
+
+export { connectToDB, disconnectFromDB };
