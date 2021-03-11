57024016dfc41001d3654d7313e37623914487a7
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-03-11 16:42:41 -0500

    Slim down logic

--- src/modules/s3.js
+++ src/modules/s3.js
@@ -4,21 +4,14 @@ import dotenv from 'dotenv';
 const REGION = 'us-east-1';
 dotenv.config();
 
-/**
- * Verifies proper AWS SDK variables are set.
- *
- * @throws {Error} When env vars are not set.
- */
-const preCheck = () => {
-  if (process.env.AWS_ACCESS_KEY_ID === undefined
-    || process.env.AWS_SECRET_ACCESS_KEY === undefined) {
-    throw new Error(`
+// Validate whether env vars are set or not. (Fail-fast)
+if (process.env.AWS_ACCESS_KEY_ID === undefined
+  || process.env.AWS_SECRET_ACCESS_KEY === undefined) {
+  throw new Error(`
        You haven't set the AWS_SECRET_ACCESS_KEY or
-       the AWS_ACCESS_KEY_ID in your env file,
-       skipping AWS S3 setup...
+       the AWS_ACCESS_KEY_ID variable(s) in your env file.
     `);
-  }
-};
+}
 
 const s3 = new S3Client({ region: REGION });
 
@@ -27,11 +20,7 @@ const s3 = new S3Client({ region: REGION });
  *
  * @returns {Promise<Bucket[]>}
  */
-const getBuckets = async () => {
-  preCheck();
-
-  return s3.send(new ListBucketsCommand({}))
-    .then(result => result.Buckets);
-};
+const getBuckets = async () => s3.send(new ListBucketsCommand({}))
+  .then(result => result.Buckets);
 
 export default getBuckets;
