525e623712d00b92f083ee4a658c61593d775fd7
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-03-11 16:48:25 -0500

    use falsy property for undefined env vars.

--- src/modules/s3.js
+++ src/modules/s3.js
@@ -5,8 +5,7 @@ const REGION = 'us-east-1';
 dotenv.config();
 
 // Validate whether env vars are set or not. (Fail-fast)
-if (process.env.AWS_ACCESS_KEY_ID === undefined
-  || process.env.AWS_SECRET_ACCESS_KEY === undefined) {
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
   throw new Error(`
        You haven't set the AWS_SECRET_ACCESS_KEY or
        the AWS_ACCESS_KEY_ID variable(s) in your env file.
