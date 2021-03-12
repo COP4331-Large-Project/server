cdaf589f5330494b711a826be27aa9342b8d565b
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-11 19:44:22 -0500

    added tests for upload and list

--- src/test/S3.test.js
+++ src/test/S3.test.js
@@ -1,4 +1,4 @@
-import getBuckets from '../modules/s3.js';
+import { getBuckets, listObjects, uploadObject } from '../modules/s3.js';
 import { logger } from '../modules/globals.js';
 
 describe('#getBuckets', () => {
@@ -8,5 +8,26 @@ describe('#getBuckets', () => {
     // Print Buckets
     logger.info('Bucket List:');
     buckets.forEach(bucket => logger.info(bucket.Name));
+  });
+});
+
+describe('#listObjects', () => {
+  it('should retrieve all the objects in the specified bucket', async () => {
+    // Get objects
+    const objects = await listObjects();
+    // Print items
+    logger.info('Objects:');
+    objects.forEach(item => logger.info(item.Key));
+  });
+});
+
+describe('#uploadObject', () => {
+  it('should upload globals.js into foo/', async () => {
+    const data = uploadObject({
+      Key: 'globals.js',
+      Path: 'foo/',
+      File: '../modules/globals.js',
+    });
+    logger.info(data);
   });
 });
