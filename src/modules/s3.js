663408bc3201b9dca2cf4168f29165a8c0f5655f
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-11 19:49:35 -0500

    Added functions for upload and list

--- src/modules/s3.js
+++ src/modules/s3.js
@@ -1,5 +1,14 @@
-import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  ListBucketsCommand,
+  ListObjectsCommand,
+  PutObjectCommand,
+} from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
+import assert from 'assert';
+import fs from 'fs';
+import path from 'path';
+import { __dirname } from './globals.js';
 
 const REGION = 'us-east-1';
 dotenv.config();
@@ -22,4 +31,32 @@ const s3 = new S3Client({ region: REGION });
 const getBuckets = async () => s3.send(new ListBucketsCommand({}))
   .then(result => result.Buckets);
 
-export default getBuckets;
+/**
+ * @param param
+ * @param {String} param.Bucket The name of the bucket
+ * @returns {Promise<Object[]>}
+ */
+const listObjects = async (param) => s3.send(new ListObjectsCommand({ Bucket: param.Bucket || 'image-sharing-prject' })).then(result => result.Contents);
+
+/**
+ * @param payload
+ * @param {String} payload.Bucket The name of the bucket
+ * @param {String} payload.Key The name of the file to be upload
+ * @param {String} payload.Path The parent directory to put the file in
+ * @param {String} payload.File The file to be uploaded
+ * @returns {Promise<PutOBjectCommandOuput>}
+ */
+const uploadObject = async (payload) => {
+  assert(payload.Key !== undefined);
+  assert(payload.File !== undefined);
+  const contents = fs.readFileSync(path.resolve(__dirname, payload.File));
+  const filePath = payload.Path || '';
+  const input = {
+    Bucket: payload.Bucket || 'image-sharing-project',
+    Key: `${filePath}${payload.Key}`,
+    Body: contents,
+  };
+  return s3.send(new PutObjectCommand(input));
+};
+
+export { getBuckets, listObjects, uploadObject };
