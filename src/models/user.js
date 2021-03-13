f8f11989465c6c29cfad4923fe21a2841ebe587e
Armintron <64503268+Armintron@users.noreply.github.com>
2021-03-13 02:24:26 -0500

    "unique" comment

--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,8 @@ const modelName = 'User';
 const userSchema = new mongoose.Schema({
   FirstName: String,
   LastName: String,
+  // "unique" may cause unexpected results if its value differs from the collection's index,
+  // but is included for readability in the case that it doesnt
   Username: { type: String, required: true, unique: true },
   Password: { type: String, required: true },
 });
