0f8d482b50f28e9923dcca8ac126ca0c40eb91e5
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-03-17 23:20:34 -0400

    Added merger

--- src/controllers/group.js
+++ src/controllers/group.js
@@ -1,4 +1,7 @@
+import mongoose from 'mongoose';
 import GroupModel from '../models/group';
+
+const { ObjectId } = mongoose.Types;
 
 const Group = {
   register: async (req, res) => {
@@ -17,6 +20,26 @@ const Group = {
     } catch (err) {
       res.status(500).send('TODO ERROR');
     }
+  },
+
+  join: async (req, res) => {
+    const { inviteCode } = req.params;
+    const group = await Group.findOne({ inviteCode }).populate(Group.fieldsToPopulate);
+    if (group === null) {
+      return res.status(404).send({ message: 'TODO ERROR: GROUP DOES NOT EXIST' });
+    }
+    if (group.publicGroup) {
+      return res.send({ group, message: 'SUCCESSFULLY JOINED PUBLIC GROUP' });
+    }
+    if (!ObjectId.isValid(req.body.user)) {
+      return res.status(404).send({ message: 'TODO ERROR: BAD USER ObjectId' });
+    }
+    const user = ObjectId(req.body.user);
+    const authorizedUser = (group.users).some(x => x.equals(user));
+    if (group.creator.equals(user) || authorizedUser) {
+      return res.send({ group, message: 'SUCCESSFULLY AUTHENTICATED' });
+    }
+    return res.status(404).send({ message: 'TODO ERROR: USER DOES NOT HAVE PERMISSION' });
   },
 
   // TODO: invite code generation
