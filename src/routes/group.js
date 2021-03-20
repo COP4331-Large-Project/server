954d62d8e4fcc5517ba1e25242f403f5732d4a52
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-03-19 22:04:39 -0400

    added swagger support

--- src/routes/group.js
+++ src/routes/group.js
@@ -1,11 +1,45 @@
+/**
+ * @swagger
+ * tags:
+ *    name: Group
+ *    description: API to manage groups.
+ */
+
 import express from 'express';
 import Group from '../controllers/group';
 
 const groups = express.Router();
 
-// TODO: Add Swagger Docs
-
-// add a new group
+/**
+ * @swagger
+ * path:
+ * /groups/:
+ *    post:
+ *      description: Creates a new group
+ *      summary: Creates a group
+ *      tags:
+ *        - Group
+ *
+ *      requestBody:
+ *        description:
+ *        required: true
+ *
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: OK
+ *        404:
+ *          description: Username/password combination is incorrect
+ *        500:
+ *          description: Internal error
+ */
 groups.post('/', Group.register);
 
 // join an existing group, given by :inviteCode
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,18 +1,96 @@
+/**
+ * @swagger
+ * tags:
+ *    name: User
+ *    description: API to manage users.
+ */
+
 import express from 'express';
 import User from '../controllers/user';
 
 const router = express.Router();
 
-// TODO: Add Swagger Docs
-
-// add a new user
+/**
+ * @swagger
+ * path:
+ * /users/:
+ *    post:
+ *      description: Creates a new user
+ *      summary: Creates a user
+ *      tags:
+ *        - User
+ *
+ *      requestBody:
+ *        description:
+ *        required: true
+ *
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                username:
+ *                  type: string
+ *                  example: jbrown
+ *                password:
+ *                  type: string
+ *                  example: password12345
+ *                firstName:
+ *                  type: string
+ *                  example: John
+ *                lastName:
+ *                  type: string
+ *                  example: Brown
+ *
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: OK
+ *        404:
+ *          description: Username/password combination is incorrect
+ *        500:
+ *          description: Internal error
+ */
 router.post('/', User.register);
 
-// TODO: Add Swagger Docs
-
+/**
+ * @swagger
+ * path:
+ * /users/login:
+ *    post:
+ *      description: Logs in an existing user
+ *      summary: Logs in a user
+ *      tags:
+ *        - User
+ *
+ *      requestBody:
+ *        description:
+ *        required: true
+ *
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                username:
+ *                  type: string
+ *                  example: jbrown
+ *                password:
+ *                  type: string
+ *                  example: password12345
+ *
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: OK
+ *        404:
+ *          description: Username/password combination is incorrect
+ *        500:
+ *          description: Internal error
+ */
 router.post('/login', User.login);
-
-// TODO: Add Swagger Docs
 
 router.delete('/:userID', User.delete);
 
