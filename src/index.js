dbbaa6449d67f78c54902e239d7ae6f9420a9a2c
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-03-31 18:12:24 -0400

    Initialized server socket

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { logger } from './globals';
 import initWebServer from './services/webServer';
-import io from './services/Socket';
+import Socket from './services/Socket';
 
 (async function main() {
   // Load in environment variables from file.
@@ -12,11 +12,9 @@ import io from './services/Socket';
     logger.info('Server is running on http://localhost:5000');
   }
 
-  // Initialize Socket.io
-  const httpServer = http.createServer(app);
-  io.attach(httpServer);
-  httpServer.listen(3000);
-
   // Start listening for webserver connections.
   app.listen(process.env.PORT || 5000);
+
+  // Initialize socket
+  Socket.initSocket(app);
 }());
