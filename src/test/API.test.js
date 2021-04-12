3ac01a9e064a11cac2fca702f241f42f7f2794ab
suneettipirneni <suneettipirneni@icloud.com>
2021-04-12 18:19:17 -0400

    add name back and don't force exit

--- src/test/API.test.js
+++ src/test/API.test.js
@@ -75,7 +75,7 @@ describe('Group API Methods', () => {
     const res = await request(app)
       .post('/groups')
       .send({
-        creator: userPayload.id, publicGroup: true, emails: [],
+        creator: userPayload.id, name: 'My group Name', publicGroup: true, emails: [],
       })
       .expect('Content-Type', /json/)
       .expect(200);
