59f58283a93e5b354c215488f65da06efbf56ffb
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-04-13 10:34:52 -0400

    Added test for sendgrid and verification

--- src/test/API.test.js
+++ src/test/API.test.js
@@ -2,10 +2,13 @@ import request from 'supertest';
 import { v4 as uuidv4 } from 'uuid';
 import mongoose from 'mongoose';
 import initWebServer from '../services/webServer';
+import SendGrid from '../services/SendGrid';
 import UserModel from '../models/user';
 import GroupModel from '../models/group';
 
 let app;
+
+jest.setTimeout(30000);
 
 // Random username generation
 const username = uuidv4();
@@ -24,14 +27,13 @@ let groupPayload;
 // Initialize the web app.
 beforeAll(async () => {
   app = await initWebServer(app);
-}, 20000);
+});
 
-afterAll(async (done) => {
+afterAll(async () => {
   await UserModel.findOneAndDelete({ _id: userPayload.id });
   await GroupModel.findOneAndDelete({ inviteCode: groupPayload.inviteCode });
   // Shut down web server.
   await mongoose.connection.close();
-  done();
 });
 
 describe('User API methods', () => {
@@ -51,7 +53,20 @@ describe('User API methods', () => {
       .expect(201);
 
     userPayload.id = response.body.id;
+    userPayload.verificationCode = response.body.verificationCode;
     delete response.body.imgURL;
+    expect(response.body).toMatchObject(userPayload);
+  });
+
+  test('Verifying user', async () => {
+    const response = await request(app)
+      .post(`/users/${userPayload.id}/verify`)
+      .send({
+        verificationCode: userPayload.verificationCode,
+      })
+      .expect('Content-Type', /json/)
+      .expect(200);
+
     expect(response.body).toMatchObject(userPayload);
   });
 
@@ -73,7 +88,9 @@ describe('Group API Methods', () => {
   test('Creating new group', async () => {
     const res = await request(app)
       .post('/groups')
-      .send({ creator: userPayload.id })
+      .send({
+        creator: userPayload.id, name: 'My group Name', publicGroup: true, emails: [],
+      })
       .expect('Content-Type', /json/)
       .expect(200);
 
@@ -84,10 +101,24 @@ describe('Group API Methods', () => {
 
   test('Join new group', async () => {
     const res = await request(app)
-      .post(`/groups/join/${groupPayload.inviteCode}`)
+      .post(`/groups/${groupPayload.inviteCode}/join`)
       .send({ user: userPayload.id })
       .expect(204);
 
     groupPayload.invitedUsers = res.body.invitedUsers;
+  });
+});
+
+describe('SendGrid Test', () => {
+  const message = {
+    to: 'no-reply@imageus.io',
+    from: 'no-reply@imageus.io',
+    subject: 'Test',
+    text: 'This is a test email',
+  };
+  test('Send mail', async () => {
+    const response = await SendGrid.sendMessage(message);
+
+    expect(response);
   });
 });
